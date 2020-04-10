35a2fe1fa52f0a30da9e262805b640d1c1a6741f
jay <johnjohnsonokah@gmail.com>
2020-04-10 08:59:37 +0100

    Replace generics and urlpatterns  with Viewsets and Routers

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail, UserDetail, UserList
+from .views import PostViewSet, UserViewSet
 
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+router = SimpleRouter()
+router.register('', PostViewSet, base_name='posts')
+router.register('users', UserViewSet, base_name='users')
+
+urlpatterns = router.urls
